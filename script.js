acea85c804b3be24f995dc52ea3688e490fea3d3
John Francis <johnfrancis95815@gmail.com>
2020-05-18 23:02:44 +0530

    removed bugs

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const headers = { Authorization: "Basic " + btoa("5182N78dxnDwgjP1Jg") };
 
-let tickets_loaded = false;
 async function view_ticket_list() {
-  if (tickets_loaded) return;
   let url =
-    "https://johnspyboy.freshdesk.com/api/v2/tickets?include=description,requester";
+    "https://johnspyboy.freshdesk.com/api/v2/tickets?include=description,requester&order_by=status";
   let data = await fetch(url, {
     method: "GET",
     headers,
@@ -26,7 +24,8 @@ async function view_ticket_list() {
         e["subject"],
         e["description_text"],
         e["due_by"],
-        e["priority"]
+        e["priority"],
+        e["status"]
       )
     );
   });
@@ -39,7 +38,8 @@ function create_ticket_card(
   subject,
   description,
   dueDate,
-  priority_no
+  priority_no,
+  status_no
 ) {
   description = description ? description : "no details";
   let card = document.createElement("div");
@@ -51,14 +51,23 @@ function create_ticket_card(
     3: ["bg-warning", "high"],
     4: ["bg-danger", "urgent"],
   };
+  let status = {
+    2: "open",
+    3: "pending",
+    4: "resolved",
+    5: "closed",
+    6: "waiting for customer",
+    7: "waiting for third-party",
+  };
   card.innerHTML = `
   <div class="card-header " >
   <div class="d-inline-flex justify-content-between" style="width:100%" >
-   <p>${requester["name"]} </p><p class=" ${
+   <div><p>${requester["name"]} </p></div><div><p class=" ${
     priority[priority_no][0]
-  } rounded text-white p-2">priority : ${priority[priority_no][1]}</p>
+  } rounded text-white p-2">priority : ${priority[priority_no][1]}</p></div>
   </div>
-  <small>${requester["email"]}</small>
+  <small><u>Status: ${status[status_no]}</u></small><br/>
+  <small class=" text-dark font-italic py-1 px-2">${requester["email"]}</small>
   </div>
   
   <div class="card-body">
@@ -71,22 +80,31 @@ function create_ticket_card(
         <p>Details:</p>
         <p style="max-height:150px; overflow-y: scroll; ">"${description}"</p>
         <form>
-            <div class="form-group row">
-            <label for="Details${Id}" class="col col-form-label">Details</label>
-            <input type="text" class="form-control" id="Details${Id}">
-            </div>
+           
             <div class="form-group row">
             <label for="Priority${Id}"class="col col-form-label">Priority</label>
              <select class="custom-select mr-sm-2" id="Priority${Id}">
-                <option selected>Select Priority</option>
+             <option value="" selected> Select Priority</option>
                 <option value="1">Low</option>
                 <option value="2">Medium</option>
                 <option value="3">High</option>
                 <option value="4">Urgent</option>
             </select>
             </div>
+            <div class="form-group row">
+            <label for="Status${Id}"class="col col-form-label">Status</label>
+             <select class="custom-select mr-sm-2" id="Status${Id}">
+             <option value="" selected>Select Status</option>
+                <option value="2">Open</option>
+                <option value="3">Pending</option>
+                <option value="4">Resolved</option>
+                <option value="5">Closed</option>
+                <option value="6">Waiting for Customer</option>
+                <option value="7">Waiting for Third-party</option>
+            </select>
+            </div>
             <div class="form-group row text-center">
-            <button type="btn" class="btn btn-success mr-3" onclick="update_ticket(${Id},'${description}',${priority_no})">Update</button>
+            <button type="btn" class="btn btn-success mr-3" onclick="update_ticket(${Id},${priority_no},${status_no})">Update</button>
             <p>OR</p>
             <button type="btn" class="btn btn-danger ml-3" onclick="delete_ticket(${Id})">Delete</button>
             </div>
@@ -97,15 +115,16 @@ function create_ticket_card(
   return card;
 }
 
-async function update_ticket(ticketId, def_description, def_priority) {
+async function update_ticket(ticketId, def_priority, def_status) {
   let url = "https://johnspyboy.freshdesk.com/api/v2/tickets/" + ticketId;
   let priority = document.getElementById(`Priority${ticketId}`).value;
   priority = priority ? priority : def_priority;
-  let details = document.getElementById(`Details${ticketId}`).value;
-  details = details ? details : def_description;
+  let status = document.getElementById(`Status${ticketId}`).value;
+  status = status ? status : def_status;
+
   let formData = new FormData();
   formData.append("priority", priority);
-  formData.append("description", details);
+  formData.append("status", status);
   let data = await fetch(url, {
     method: "PUT",
     headers,
@@ -117,7 +136,7 @@ async function update_ticket(ticketId, def_description, def_priority) {
     .json()
     .catch(() => console.error("error in parsing"));
   console.log(parsedData);
-  location.reload();
+  view_ticket_list();
 }
 
 async function delete_ticket(ticketId) {
@@ -130,7 +149,7 @@ async function delete_ticket(ticketId) {
   });
 
   console.log("deleted");
-  location.reload();
+  view_ticket_list();
 }
 
 view_ticket_list();
